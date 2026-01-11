07174e0e294febc2dd7e0f417eb79f91f46ecd1b
Fleeting <Macro__9@outlook.com>
2026-01-11 13:35:35 +0800

    塞命令行里面

--- lore_for_collection_min/src/main.rs
+++ lore_for_collection_min/src/main.rs
@@ -1,3 +1,4 @@
+use std::{env};
 use std::path::Path;
 use crate::input_lore::input_lore_file;
 use crate::input_lore::parse;
@@ -9,13 +10,15 @@ mod input_lore;
 mod output;
 
 fn main() {
-    let content: String = input_lore_file(Path::new("..\\test\\basic.lore"));
+    let args: Vec<String> = env::args().collect();
+
+    let content: String = input_lore_file(Path::new(&args[1]));
     let target = parse(content);
-    
+
     for line in &target {
         println!("{}", line);
     }
-    
-    output_html("Test", target, Path::new("..\\test\\test.html"));
-    
+
+    output_html("Test", target, Path::new(&args[2]));
+
 }
